7eb199a03fcb56199121bde300f500293d60febe
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-04-29 17:18:38 -0400

    progress

--- src/main/java/com/redhat/summit2019/HttpApplication.java
+++ src/main/java/com/redhat/summit2019/HttpApplication.java
@@ -14,26 +14,63 @@ public class HttpApplication extends AbstractVerticle {
   static final String template = "Hello, %s!";
 
   @Override
-  public void start(Future<Void> future) {
+  public void start(Future<Void> startFuture) {
+
+    Future init = loadAdjectives().compose(v -> startHttpServer());
+    init.setHandler(startFuture.completer());
+
+  }
+
+  private Future<Void> startHttpServer(){
+    Future<Void> future = Future.future();
+
     // Create a router object.
     Router router = Router.router(vertx);
 
     router.get("/api/greeting").handler(this::greeting);
+    router.get("/api/adjective").handler(this::adjectiveHandler);
     router.get("/*").handler(StaticHandler.create());
 
     // Create the HTTP server and pass the "accept" method to the request handler.
     vertx
-        .createHttpServer()
-        .requestHandler(router)
-        .listen(
-            // Retrieve the port from the configuration, default to 8080.
-            config().getInteger("http.port", 8080), ar -> {
-              if (ar.succeeded()) {
-                System.out.println("Server started on port " + ar.result().actualPort());
-              }
-              future.handle(ar.mapEmpty());
-            });
+            .createHttpServer()
+            .requestHandler(router)
+            .listen(
+                    // Retrieve the port from the configuration, default to 8080.
+                    config().getInteger("http.port", 8080), ar -> {
+                      if (ar.succeeded()) {
+                        System.out.println("Server started on port " + ar.result().actualPort());
+                        future.complete();
+                      }else{
+                        future.fail(ar.cause());
+                      }
+                    });
 
+    return future;
+  }
+
+  private Future<Void> loadAdjectives() {
+    Future<Void> future = Future.future();
+    vertx.fileSystem().readFile("adjectives.txt", res -> {
+      if (res.succeeded()) {
+        System.out.println(res.result());
+        future.complete();
+      } else {
+        System.err.println("Oh oh ..." + res.cause());
+        future.fail(res.cause());
+      }
+    });
+    return future;
+  }
+
+  private void adjectiveHandler(RoutingContext rc) {
+
+    JsonObject response = new JsonObject()
+            .put("adjective", "clapper-clawed");
+
+    rc.response()
+            .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+            .end(response.encodePrettily());
   }
 
   private void greeting(RoutingContext rc) {
